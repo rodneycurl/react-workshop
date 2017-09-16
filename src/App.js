0e14a26f52314709c92c1a6a6ebeb643700d2acd
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-09-16 10:49:44 -0400

    adding content

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react'
 
 function App() {
+  const product = {
+    name: "JavaScript: The Definitive Guide",
+    description: `Since 1996, JavaScript: The Definitive Guide has been the bible for JavaScript programmers`,
+    price: 28.89
+  }
+
   return (
-    <h1>Hello DevMeetings!</h1>
+    <section>
+      <h1>Hello DevMeetings!</h1>
+
+    <div>
+      <header>{ product.name }</header>
+      <p>
+        { product.description }
+      </p>
+      <span>${ product.price }</span>
+    </div>
+    </section>
   )
 }
 
