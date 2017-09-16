e8f929f60e08184fe32ae257662661210c010b0f
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-09-16 11:49:55 -0400

    added components folder for Product.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import React from 'react'
+import ProductTile from './components/Product'
 
 function App() {
-  const product = {
-    name: "JavaScript: The Definitive Guide",
-    description: `Since 1996, JavaScript: The Definitive Guide has been the bible for JavaScript programmers`,
-    price: 28.89
+  const products = [
+    {
+      id: 0,
+      name: "JavaScript: The Definitive Guide",
+      description: `Since 1996, JavaScript: The Definitive Guide has been the bible for JavaScript programmers.`,
+      price: 28.89
+  },
+  {
+    id: 1,
+    name: "Eloquent JavaScript",
+    description: `JavaScript lies at the heart of almost every modern web application...`,
+    price: 21.22
+  },
+  {
+    id: 2,
+    name: "JavaScript: The Good Parts",
+    description: `Most programming languages contain good and bad parts, but JavaScript...`,
+    price: 16.59
   }
+]
 
   return (
     <section>
-      <h1>Hello DevMeetings!</h1>
-
-    <div>
-      <header>{ product.name }</header>
-      <p>
-        { product.description }
-      </p>
-      <span>${ product.price }</span>
-    </div>
+      <h1>Products</h1>
+      { products.map(product =>
+        <ProductTile key={ product.id }
+                     item={ product } />
+      )}
     </section>
   )
 }
